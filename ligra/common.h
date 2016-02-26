6c5e39266735e934ddaf7d6bd79218233e7b59a3
Laxman Dhulipala <ldhulipa@andrew.cmu.edu>
2016-02-26 17:57:17 -0500

    fix gcc warning

--- ligra/common.h
+++ ligra/common.h
@@ -7,13 +7,13 @@ public:
   virtual inline bool update (uintE s, uintE d) { //Update
     return updateAtomic(s,d);
   }
-  virtual inline bool updateAtomic (uintE s, uintE d) = 0; //atomic version of Update
+  virtual bool updateAtomic (uintE s, uintE d) = 0; //atomic version of Update
   //cond function checks if vertex has been visited yet
 #else
   virtual inline bool update (uintE s, uintE d, intE w) { //Update
     return updateAtomic(s,d,w);
   }
-  virtual inline bool updateAtomic (uintE s, uintE d, intE w) = 0; //atomic version of Update
+  virtual bool updateAtomic (uintE s, uintE d, intE w) = 0; //atomic version of Update
   //cond function checks if vertex has been visited yet
 #endif
   virtual inline bool cond (uintE d) { return 1; } 
