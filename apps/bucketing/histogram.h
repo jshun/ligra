535899a68677d6a66491ba0fe29ce30b3dcdec72
Julian Shun <jshun@aware.aladdin.cs.cmu.edu>
2018-08-27 22:27:15 -0400

    sequential hash table power of 2

--- apps/bucketing/histogram.h
+++ apps/bucketing/histogram.h
@@ -47,6 +47,7 @@ namespace pbbs {
     }
     hist_table() {}
 
+    //size needs to be a power of 2
     void resize(size_t req_size) {
       if (req_size > size) {
         free(table);
@@ -67,8 +68,9 @@ namespace pbbs {
   template <class E, class O, class K, class V, class A, class Reduce, class Apply>
   inline pair<size_t, O*> seq_histogram_reduce(A& get_elm, size_t n, Reduce& reduce_f, Apply& apply_f, hist_table<K, V>& ht) {
     typedef tuple<K, V> KV;
-    ht.resize(n);
-    sequentialHT<K, V> S(ht.table, n, 1.0f, ht.empty);
+    long size = 1 << pbbs::log2_up(n+1);
+    ht.resize(size);
+    sequentialHT<K, V> S(ht.table, size, 1.0f, ht.empty);
     for (size_t i=0; i<n; i++) {
       E a = get_elm(i);
       reduce_f(S, a);
