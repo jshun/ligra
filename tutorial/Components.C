94e8a38259c5134d7b53881367582b1230cb2043
Julian Shun <jshun@aware.aladdin.cs.cmu.edu>
2019-08-13 22:02:01 -0400

    changes

--- tutorial/Components.C
+++ tutorial/Components.C
@@ -25,8 +25,8 @@ void Compute(graph<vertex>& GA, commandLine P) {
   vertexSubset Frontier(n,n,frontier); //initial frontier contains all vertices
 
   while(!Frontier.isEmpty()){ //iterate until IDS converge
-    vertexSubset output = edgeMap(GA, Frontier, CC_F(IDs),
-				  GA.m/20,DENSE,true); //tells edgemap to remove duplicates
+    vertexSubset output = edgeMap(GA, Frontier, CC_F(IDs), GA.m/20,
+				  remove_duplicates); //tells edgemap to remove duplicates
     Frontier.del();
     Frontier = output;
   }
